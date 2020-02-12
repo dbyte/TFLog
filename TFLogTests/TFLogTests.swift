b3ff339beeaff9a9630eb5be571c0275a61e50f9
T. Fornalik <it-github@fornalik.de>
2020-02-12 23:36:22 +0100

    Adding Unit Test Suite

---
+++ TFLogTests/TFLogTests.swift
@@ -0,0 +1,33 @@
+//
+//  TFLogTests.swift
+//  TFLogTests
+//
+//  Created by Tammo on 12.02.20.
+//  Copyright © 2020 dbyte. All rights reserved.
+//
+
+import XCTest
+
+class TFLogTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
