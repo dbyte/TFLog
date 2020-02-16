1baacdb1e162cbfb64a85ed58b081d3a9423bc9d
T. Fornalik <it-github@fornalik.de>
2020-02-16 05:29:13 +0100

    File added

---
+++ TFLog/Source/LogLevelSymbols.swift
@@ -0,0 +1,89 @@
+//
+//  LogLevelSymbols.swift
+//  TFLog
+//
+//  Created by Tammo on 16.02.20.
+//  Copyright © 2020 dbyte. All rights reserved.
+//
+
+// MARK: - Log level unicode symbols
+
+// Note: Type needs to be public for the public result of instance method LogLevelSymbolBuilder.build() .
+/// Log level unicode symbols customization can be done via `LogLevelSymbolBuilder` followed by injection
+/// into instance method `LogConfiguration.replaceSymbols(:)`
+/// - See also: `LogLevelSymbolBuilder`
+public struct LogLevelSymbols {
+    
+    internal var error: String
+    internal var warning: String
+    internal var success: String
+    internal var action: String
+    internal var canceled: String
+    internal var other: String
+    
+    // Set default log level symbols
+    internal init() {
+        self.error = "📕"
+        self.warning = "📙"
+        self.success = "📗"
+        self.action = "📘"
+        self.canceled = "📓"
+        self.other = "📔"
+    }
+}
+
+// MARK: - Builder for replacing log level unicode symbols
+
+/// Log level unicode symbols customization.
+///
+/// Use this builder to create a new set of symbols or just set the ones you want to overwrite
+/// and afterwards inject it via `LogConfiguration.replaceSymbols(:)` .
+/// - See also: `LogConfiguration`
+public class LogLevelSymbolBuilder {
+    
+    private var logLevelSymbols: LogLevelSymbols
+    
+    public init(forConfiguration config: LogConfiguration) {
+        logLevelSymbols = config.logLevelSymbols
+    }
+    
+    @discardableResult
+    public func setError(_ error: String) -> LogLevelSymbolBuilder {
+        logLevelSymbols.error = error
+        return self
+    }
+    
+    @discardableResult
+    public func setWarning(_ warning: String) -> LogLevelSymbolBuilder {
+        logLevelSymbols.warning = warning
+        return self
+    }
+    
+    @discardableResult
+    public func setSuccess(_ success: String) -> LogLevelSymbolBuilder {
+        logLevelSymbols.success = success
+        return self
+    }
+    
+    @discardableResult
+    public func setAction(_ action: String) -> LogLevelSymbolBuilder {
+        logLevelSymbols.action = action
+        return self
+    }
+    
+    @discardableResult
+    public func setCanceled(_ canceled: String) -> LogLevelSymbolBuilder {
+        logLevelSymbols.canceled = canceled
+        return self
+    }
+    
+    @discardableResult
+    public func setOther(_ other: String) -> LogLevelSymbolBuilder {
+        logLevelSymbols.other = other
+        return self
+    }
+    
+    public func build() -> LogLevelSymbols {
+        return logLevelSymbols
+    }
+}
