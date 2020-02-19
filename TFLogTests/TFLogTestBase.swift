277d9cd9f96f11b869e25e2e38bb4498ff005de7
T. Fornalik <it-github@fornalik.de>
2020-02-19 22:38:12 +0100

    Adding TFLogTestBase.swift

---
+++ TFLogTests/TFLogTestBase.swift
@@ -0,0 +1,19 @@
+//
+//  TFLogTestBase.swift
+//  TFLogTests
+//
+//  Created by Tammo on 12.02.20.
+//  Copyright © 2020 dbyte. All rights reserved.
+//
+
+import XCTest
+
+class TFLogTestBase: XCTestCase {
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+}
