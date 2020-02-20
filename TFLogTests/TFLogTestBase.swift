ab768deed3f985975ba37fbf9c415f76efd88cf4
T. Fornalik <it-github@fornalik.de>
2020-02-20 18:24:43 +0100

    added new var timeout

--- TFLogTests/TFLogTestBase.swift
+++ TFLogTests/TFLogTestBase.swift
@@ -9,11 +9,31 @@
 import XCTest
 
 class TFLogTestBase: XCTestCase {
+    
+    var timeout: TimeInterval!
+    
+    override class func setUp() {
+        super.setUp()
+        // This is the setUp() class method.
+        // It is called before the FIRST test method begins.
+        // Set up any overall initial state here.
+    }
+    
+    override class func tearDown() {
+        // This is the tearDown() class method.
+        // It is called after ALL test methods complete.
+        // Perform any overall cleanup here.
+        super.tearDown()
+    }
 
     override func setUp() {
+        super.setUp()
+        timeout = 3
     }
 
     override func tearDown() {
+        timeout = 0
+        super.tearDown()
     }
 
 }
